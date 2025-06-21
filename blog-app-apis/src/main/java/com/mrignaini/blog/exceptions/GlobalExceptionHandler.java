2aa73083af2124911c8147dfaea10327fc74dd13
zhritik007 <ghritiksingh21@gmail.com>
2025-06-21 22:07:50 +0530

    MethodArgumentNotValidException

--- blog-app-apis/src/main/java/com/mrignaini/blog/exceptions/GlobalExceptionHandler.java
+++ blog-app-apis/src/main/java/com/mrignaini/blog/exceptions/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -22,9 +23,13 @@ public class GlobalExceptionHandler {
 	}
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<Map<String, String>> handleMethodArgsNotValidException(MethodArgsNotValidException ex){
+	public ResponseEntity<Map<String, String>> handleMethodArgsNotValidException(MethodArgumentNotValidException ex){
 		Map<String,String> resp=new HashMap<>();
-		ex.
+		ex.getBindingResult().getAllErrors().forEach(error->{
+			String fieldName=((FieldError)error).getField();
+			String message= error.getDefaultMessage();
+			resp.put(fieldName, message);
+		});
 		
 		return new ResponseEntity<Map<String,String>>(resp,HttpStatus.BAD_REQUEST);
 	}
